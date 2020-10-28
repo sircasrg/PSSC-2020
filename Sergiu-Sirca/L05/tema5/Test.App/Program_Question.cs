e21b8db9c08826c595ed16cc620778261f3905df
Sergiu <sergiu@pop-os.localdomain>
2020-10-28 20:10:46 +0200

    tema5

---
+++ Sergiu-Sirca/L05/tema5/Test.App/Program_Question.cs
@@ -0,0 +1,51 @@
+﻿using Question.Domain.CreateNewQuestionWorkflow;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using LanguageExt;
+using static Question.Domain.CreateNewQuestionWorkflow.CreateNewQuestionResult;
+using static Question.Domain.CreateNewQuestionWorkflow.Question_Description;
+
+namespace Test.App
+{
+    class Program_Question
+    {
+        static void Main(string[] args)
+        {
+            var questionResult = UnpublishedQuestion.Create("What is a network?", new List<string>() {"network"});
+
+
+            questionResult.Match(
+                    Succ: question =>
+                    {
+                        VoteQuestionButton(question);
+                        Console.WriteLine("Can vote!");
+                        return Unit.Default;
+                    },
+                    Fail: ex =>
+                    {
+                        Console.WriteLine($"Question could not be published. Reason: {ex.Message}");
+                        return Unit.Default;
+                    }
+                );
+            Console.ReadLine();
+        }
+        private static void VoteQuestionButton(UnpublishedQuestion question)
+        {
+            var publishedQuestionResult = new PublishQuestionService().PublishQuestion(question);
+            publishedQuestionResult.Match(
+                    QuestionVote=>
+                    {
+                        new VoteQuestionService().SendPermisiuneToVote(QuestionVote);
+                        return Unit.Default;
+                    },
+                    ex =>
+                    {
+                        Console.WriteLine("Can not vote!");
+                        return Unit.Default;
+                    }
+                );
+        }
+
+    }
+}
